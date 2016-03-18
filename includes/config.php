c2e2cd95ba0444324dbb8c4d21b2a53c1631836e
TaxClassRoom <office@amdata.com>
2016-03-18 13:08:29 -0500

    Intial Commit

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "roots");
+define("DB_PASS", "root");
+define("DB_NAME", "registration");
+
+  ?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php 
+include ('includes/config.php');
+include ('includes/db.php');
+$a=3;
+$b=4;
+$c=$a+$b;
+echo $c;
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="index.php">Login</a></li>
+            <li><a href="Register.php">Registered</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form method="post" style="margin-top:35px;">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" name="email" class="form-control" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" name="password" class="form-control"  placeholder="Password">
+  </div>
+<!--   <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile">
+    <p class="help-block">Example block-level help text here.</p>
+  </div> -->
+  <div class="checkbox">
+    <label>
+      <input type="checkbox" name="remember_me"> Remember Me
+    </label>
+  </div>
+  <button type="submit" name="login" class="btn btn-default">Login</button>
+</form>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+	<!--  IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> --> 
+  </body>
+</html>
